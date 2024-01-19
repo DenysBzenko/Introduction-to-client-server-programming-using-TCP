39e98781eca3687965208d9ed088681dd805f7bb
lizookk <119534908+DenysBzenko@users.noreply.github.com>
2024-01-19 09:34:51 +0200

    Works 'LIST' 'GET' 'INFO' 'DELETE' in client

--- client1/client/client.cpp
+++ client1/client/client.cpp
@@ -1,53 +1,107 @@
 #include <iostream>
 #include <WinSock2.h>
 #include <Ws2tcpip.h>
-// Linking the library needed for network communication
+#include <fstream>
+#include <string>
 #pragma comment(lib, "ws2_32.lib")
-int main()
-{
-	// Initialize Winsock
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	{
-		std::cerr << "WSAStartup failed" << std::endl;
-		return 1;
-	}
-	// Client configuration
-	int port = 12345;
-	PCWSTR serverIp = L"127.0.0.1";
-	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (clientSocket == INVALID_SOCKET)
-	{
-		std::cerr << "Error creating socket: " << WSAGetLastError() << std::endl;
-		WSACleanup();
-		return 1;
-	}
-	sockaddr_in serverAddr;
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(port);
-	InetPton(AF_INET, serverIp, &serverAddr.sin_addr);
-	// Connect to the server
-	if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		std::cerr << "Connect failed with error: " << WSAGetLastError() << std::endl;
-		closesocket(clientSocket);
-		WSACleanup();
-		return 1;
-	}
-	// Send data to the server
-	const char* message = "Hello, server! How are you?";
-	send(clientSocket, message, (int)strlen(message), 0);
-	// Receive the response from the server
-	char buffer[1024];
-	memset(buffer, 0, 1024);
-	int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-	if (bytesReceived > 0)
-	{
-		std::cout << "Received from server: " << buffer << std::endl;
-	}
-	// Clean up
-	closesocket(clientSocket);
-	WSACleanup();
-	return 0;
+
+void receiveFile(SOCKET clientSocket, const std::string& filename) {
+    std::streamsize size = 0;
+    recv(clientSocket, reinterpret_cast<char*>(&size), sizeof(size), 0);
+    if (size > 0) {
+        std::ofstream outFile("D:\\KSE\\paradigm\\Introduction to client-server programming using TCP\\Introduction-to-client-server-programming-using-TCP\\client1\\client\\clientfolder" + filename, std::ios::binary);
+        char buffer[1024];
+        while (size > 0) {
+            int bytesRead = recv(clientSocket, buffer, std::min<size_t>(size, sizeof(buffer)), 0);
+            outFile.write(buffer, bytesRead);
+            size -= bytesRead;
+        }
+        outFile.close();
+    }
 }
-	
+
+void sendFile(SOCKET clientSocket, const std::string& filename) {
+    std::ifstream file("D:\\KSE\\paradigm\\Introduction to client-server programming using TCP\\Introduction-to-client-server-programming-using-TCP\\client1\\client\\clientfolder"  + filename, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file" << std::endl;
+        return;
+    }
+
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    send(clientSocket, reinterpret_cast<const char*>(&size), sizeof(size), 0);
+
+    char buffer[1024];
+    while (file.read(buffer, sizeof(buffer))) {
+        send(clientSocket, buffer, file.gcount(), 0);
+    }
+
+    file.close();
+}
+
+void sendCommandAndGetResponse(SOCKET clientSocket, const std::string& command) {
+    send(clientSocket, command.c_str(), command.length(), 0);
+    char buffer[4096];
+    memset(buffer, 0, 4096);
+    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived > 0) {
+        std::cout << "Server response: " << std::string(buffer, bytesReceived) << std::endl;
+    }
+}
+
+int main() {
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        std::cerr << "WSAStartup failed: " << iResult << std::endl;
+        return 1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "socket failed with error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345);
+    InetPton(AF_INET, L"127.0.0.1", &serverAddr.sin_addr);
+
+    iResult = connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+    if (iResult == SOCKET_ERROR) {
+        std::cerr << "connect failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    while (true) {
+        std::string command;
+        std::cout << "Enter command ( 'EXIT' to quit): ";
+        std::getline(std::cin, command);
+
+        if (command == "EXIT") {
+            break;
+        }
+
+        if (command.rfind("GET", 0) == 0) {
+            sendCommandAndGetResponse(clientSocket, command);
+            std::string filename = command.substr(4);
+            receiveFile(clientSocket, filename);
+        }
+        else if (command.rfind("PUT", 0) == 0) {
+            std::string filename = command.substr(4);
+            sendFile(clientSocket, filename);
+            sendCommandAndGetResponse(clientSocket, command);
+        }
+        else {
+            sendCommandAndGetResponse(clientSocket, command);
+        }
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
+}
